8b7aa4ad874b99261156b175a0549ac518c5d44a
moharifhidayat7 <moharifhidayat7@outlook.com>
2026-07-24 10:01:30 +0700

    feat: add GraphQL Playground landing page

--- src/integrations/graphql/graphql.module.ts
+++ src/integrations/graphql/graphql.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from '@apollo/server-plugin-landing-page-graphql-playground';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+      plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
     }),
   ],
 })
